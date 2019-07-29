9035830b01b3f6829b9ea1eaaa99f32aa5285989
Dmitry Grebenschikov <dvengroff@gmail.com>
2019-07-29 13:36:30 +0400

    nineteenth commit (new src/game-engine.php, update composer.json)

---
+++ src/game-engine.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace BrainGames\GameEngine;
+
+use function \cli\line;
+use function \cli\prompt;
+
+function run($gameName)
+{
+    line('Welcome to the Brain Games!');
+    switch ($gameName) {
+        case 'brain-games':
+            break;
+        case 'brain-even':
+            line('Answer "yes" if number even otherwise answer "no".');
+            break;
+        case 'brain-calc':
+            line('What is the result of the expression?');
+            break;
+    }
+    line();
+    $name = prompt('May I have your name?', false, " ");
+    line("Hello, %s!", $name);
+    line();
+    return $name;
+}

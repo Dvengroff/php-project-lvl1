e583913df8be4ef53d6574af3cc9143ee74e2ddc
Dmitry Grebenschikov <dvengroff@gmail.com>
2019-07-29 16:38:36 +0400

    twenty first commit (update src/game-engine.php)

--- src/game-engine.php
+++ src/game-engine.php
@@ -6,7 +6,7 @@ use function \cli\line;
 use function \cli\prompt;
 
 function run($gameName)
-{    
+{
     line('Welcome to the Brain Games!');
     switch ($gameName) {
         case 'brain-games':

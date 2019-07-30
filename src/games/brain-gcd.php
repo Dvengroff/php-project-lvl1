8ab00510c3924b8684beaa98ae8b1ecf9fe94c9d
Dmitry Grebenschikov <dvengroff@gmail.com>
2019-07-30 11:02:05 +0400

    twenty seventh commit (new brain-gcd.php, update composer.json)

---
+++ src/games/brain-gcd.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BrainGames\Games\BrainGcd;
+
+use function \BrainGames\GameEngine\run;
+
+function getGcd(int $a, int $b)
+{
+    $a = abs($a);
+    $b = abs($b);
+    while ($a !== $b) {
+        if ($a > $b) {
+            $a -= $b;
+        } else {
+            $b -= $a;
+        }        
+    }
+    return $a;
+}
+
+function playBrainGcd()
+{
+    $gameDescription = 'Find the greatest common divisor of given numbers.';
+    $task = function () {
+        $firstNum = rand(1, 99);
+        $secondNum = rand(1, 99);
+        return "{$firstNum} {$secondNum}";
+    };
+    $correctAnswer = function ($numbers) {
+        $numbersArr = explode(' ', $numbers);
+        $a = $numbersArr[0];
+        $b = $numbersArr[1];
+        return (string) getGcd($a, $b);
+    };
+
+    run($gameDescription, $task, $correctAnswer);
+}
